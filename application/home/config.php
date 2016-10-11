e423bdf6ba7fbcc51a275e3faddd09ac919d2010
wangweijin <64534264@qq.com>
2016-10-11 20:20:17 +0800

    路径问题

--- application/home/config.php
+++ application/home/config.php
@@ -2,11 +2,11 @@
 //配置文件
 return [
     'parse_str'=>[
-        '__PUBLIC__'=>'/',
-        '__ROOT__' => '../../',
-        '__CSS__'     =>  '/home/css/',
-        '__JS__'     =>  '/home/js/',
-        '__IMAGE__'     =>  '/home/image/',
+        '__PUBLIC__'=>'/SignSystem2/public/',
+        '__ROOT__' => '/',
+        '__CSS__'     =>  '/SignSystem2/public/home/css/',
+        '__JS__'     =>  '/SignSystem2/public/home/js/',
+        '__IMAGE__'     =>  '/SignSystem2/public/home/image/',
     ],
     'SCHOOLTIME' => '2016-09-06',
 ];
