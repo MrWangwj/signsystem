6719708b57266367e8d0e2fd964b8fbd5ef79188
wangwang <404174857@qq.com>
2016-10-20 20:57:08 +0800

    wang

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -4,6 +4,9 @@ namespace app\home\controller;
 use app\home\model\User;
 use Org\Util\Date;
 use think\Controller;
+use Think\Model;
+use think\Db;
+
 session_start();
 class Index extends Controller
 {
@@ -72,27 +75,15 @@ class Index extends Controller
         $this->redirect(url('index/index'));
     }
 
-    public function test(){
-        $nowday = strtotime(date('Y-m-d',time()));
-        $sum = $this->getDayTime($nowday);
-        echo dump($sum);
-//        $number_wk=date("w",time())-1;
-//        if($number_wk==-1){
-//            $number_wk=6;
-//        }
-//        $number_wOk = 7-$number_wk;
-//        $number_last = $number_wk+7;
-//        $weekStart = strtotime(date('Y-m-d',strtotime("-$number_wk day")));//本周一日期时间戳
-//        $weekOver = strtotime(date('Y-m-d',strtotime("+$number_wOk day")));//下周一日期时间戳
-//        $weekLast = strtotime(date('Y-m-d',strtotime("-$number_last day")));//上周一日期时间戳
-//        //某人本周在线时长
-//        $list = db('sign')
-//            -> join('user','user.user_id = sign.user_id')
-//            -> where('state','<>',0)
-//            ->where("sign.user_id",'20151515135')
-//            ->where("start_time","between","$weekLast,$weekStart")
-//            ->select();
-//        echo dump($list);
+    public function test()
+    {
+        $j=0;
+        $listNull = null;
+        $list = Db::query("SELECT user.`user_id`,user.`name`,sign.`start_time`,sign.`over_time`,groups.`group_name`,SUM(sign.`over_time`-sign.`start_time`)AS times FROM user LEFT JOIN sign ON(user.`user_id`=sign.`user_id`) LEFT JOIN user_group ON(user.`user_id`=user_group.`user_id`) LEFT JOIN groups ON(user_group.`group_id` = groups.`group_id`)  
+GROUP BY user.`user_id` HAVING SUM(sign.`over_time`-sign.`start_time`) ORDER BY times DESC");
+
+        echo dump($list);
     }
+
 
 }
--- application/home/controller/attendance.php
+++ application/home/controller/attendance.php
@@ -9,10 +9,14 @@ namespace app\home\controller;
 
 use app\home\model\User;
 use think\Controller;
+use think\Db;
 
 class Attendance extends Base
 {
     public function index(){
+        return $this->fetch();
+    }
+    public function test(){
         return $this->fetch();
     }
     //获取某人最后在线的时间
@@ -142,5 +146,39 @@ class Attendance extends Base
             }
             return round($sumtime/60);
         }
+    }
+
+
+
+    public function getThisTime(){
+        $number_wk=date("w",time())-1;
+        if($number_wk==-1){
+            $number_wk=6;
+        }
+        $number_wOk = 7-$number_wk;
+        $weekStart = strtotime(date('Y-m-d',strtotime("-$number_wk day")));//周一日期时间戳
+        $weekOver = strtotime(date('Y-m-d',strtotime("+$number_wOk day")));//下周一日期时间戳
+
+        $list = Db::query("SELECT user.`user_id`,user.`name`,sign.`start_time`,sign.`over_time`,groups.`group_name`,SUM(sign.`over_time`-sign.`start_time`)AS times FROM user LEFT JOIN sign ON(user.`user_id`=sign.`user_id`) LEFT JOIN user_group ON(user.`user_id`=user_group.`user_id`) LEFT JOIN groups ON(user_group.`group_id` = groups.`group_id`) 
+ where sign.`start_time`>=$weekStart and sign.`over_time`<$weekOver
+GROUP BY user.`user_id` HAVING SUM(sign.`over_time`-sign.`start_time`) ORDER BY times DESC");
+        return $list;
+    }
+    public function getNo(){
+        $j=0;
+        $listNull = null;
+        $list = Db::query("SELECT user.`user_id`,user.`name`,sign.`start_time`,sign.`over_time`,groups.`group_name` FROM user LEFT JOIN sign ON(user.`user_id`=sign.`user_id`) LEFT JOIN user_group ON(user.`user_id`=user_group.`user_id`) LEFT JOIN groups ON(user_group.`group_id` = groups.`group_id`)  
+GROUP BY user.`user_id`   ORDER BY user.`user_id` ");
+        for ($i=0;$i<count($list);$i++){
+            if(empty($list[$i]['start_time'])){
+                $listNull[$j]['user_id']=$list[$i]['user_id'];
+                $listNull[$j]['name']=$list[$i]['name'];
+                $listNull[$j]['group_name']=$list[$i]['group_name'];
+                $listNull[$j]['time'] = "--";
+                $j++;
+            }
+        }
+//        return $listNull;
+        return json(['list' => $listNull, 'count' => count($listNull)]);
     }
 }
